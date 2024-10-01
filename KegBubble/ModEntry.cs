f1c6039275b5d848698ba05214f41de5d4a6ebf6
cflsxjw <cfl.sxjw@gmail.com>
2024-10-01 20:54:37 +0800

    添加KegBubble RobinSellKeg-修复一个bug

---
+++ KegBubble/ModEntry.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace KegBubble
+{
+    internal sealed class ModEntry : Mod
+    {
+        private readonly HashSet<StardewValley.Object> _kegList = new();
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+            helper.Events.World.ObjectListChanged += OnObjectListChanged;
+            helper.Events.Display.RenderedWorld += OnRenderedWorld;
+        }
+        
+        private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
+        {
+            foreach (var location in Game1.locations)
+            {
+                foreach (var item in location.Objects.Values.Where(item => item.QualifiedItemId == "(BC)12"))
+                {
+                    _kegList.Add(item);
+                }
+            }
+        }
+
+        private void OnObjectListChanged(object? sender, ObjectListChangedEventArgs e)
+        {
+            foreach (var item in e.Added)
+            {
+                if (item.Value.QualifiedItemId == "(BC)12")
+                {
+                    _kegList.Add(item.Value);
+                }
+            }
+
+            foreach (var item in e.Removed)
+            {
+                if (item.Value.QualifiedItemId == "(BC)12")
+                {
+                    _kegList.Remove(item.Value);
+                }
+            }
+            Monitor.Log(_kegList.Count.ToString(), LogLevel.Error);
+        }
+
+        private void OnRenderedWorld(object? sender, RenderedWorldEventArgs e)
+        {
+            foreach (var item in _kegList.Where(item => item.heldObject.Value == null))
+            {
+                e.SpriteBatch.Draw(Game1.emoteSpriteSheet,
+                    new Vector2(item.TileLocation.X * Game1.tileSize - Game1.viewport.X,
+                        (item.TileLocation.Y - 1.5f) * Game1.tileSize - Game1.viewport.Y),
+                    new Rectangle(0, 64, 16, 16), Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 0f);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# cflsxjw的stardew mods - 模组列表
+# cflsxjw的 Stardew Valley mods - 模组列表
 
-## 1. autoWatering-自动浇水
+---
+
+> 这是cflsxjw的***私人mod库***，旨在**学习**和略微定制游戏体验 :P
+> 
+## 1. Auto Watering-自动浇水-SMAPI
+### 已实现功能
+- 以 ***5/地块*** 的价格为非空地块浇水
+### TODO
+- 使价格可配置
+## 2. Walk Faster-移动加速-SMAPI
+### 已实现功能
+- 将玩家速度乘1.2(提高20%)
+### TODO
+- 使速度加成可配置
+## 3. Price Adjust-价格调整-CP
+### 已实现功能
+- 传送塔： -50%
+- 黄金时钟： 10000000 -> 3000000
+- 传送法杖： 2000000 -> 850000
+### TODO
+- 使价格减少幅度可配置
+## 4. RobinSellsMachines-木匠商店售卖机器-CP
+### 已实现功能
+- 使Robin售卖各种机器
+### TODO
+- 添加机器的种类
+- 调整售卖价格
